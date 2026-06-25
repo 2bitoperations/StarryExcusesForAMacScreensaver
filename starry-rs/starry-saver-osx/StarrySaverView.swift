1efae08fbece05affafed631c7775f18f5e12651
Andrew Malota <2bitoperations@gmail.com>
2026-06-25 15:09:15 -0500

    fix(saver): mark buildCommit as .public in os_log to prevent <private> redaction

--- starry-rs/starry-saver-osx/StarrySaverView.swift
+++ starry-rs/starry-saver-osx/StarrySaverView.swift
@@ -55,7 +55,7 @@ class StarrySaverView: ScreenSaverView {
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
         animationTimeInterval = 1.0 / 60.0
-        log.info("init \(Int(frame.width))×\(Int(frame.height)) isPreview=\(isPreview) build=\(buildCommit)")
+        log.info("init \(Int(frame.width))×\(Int(frame.height)) isPreview=\(isPreview) build=\(buildCommit, privacy: .public)")
     }
 
     required init?(coder: NSCoder) {
